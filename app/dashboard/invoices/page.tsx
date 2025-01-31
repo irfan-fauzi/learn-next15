ef4767ea63151189b623d158d64ad9b8ae836ff9
irfan-fauzi <irfanfauzi24@yahoo.com>
2025-02-01 01:16:01 +0700

    start pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,26 +1,40 @@
-import { lusitana } from "@/app/ui/fonts"
-import { CreateInvoice } from "@/app/ui/invoices/buttons"
-import Search from "@/app/ui/search"
+import { fetchInvoicesPages } from "@/app/lib/data";
+import { lusitana } from "@/app/ui/fonts";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Pagination from "@/app/ui/invoices/pagination";
+import InvoicesTable from "@/app/ui/invoices/table";
+import Search from "@/app/ui/search";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
 
-const InvoicePage = () => {
-  
+const InvoicePage = async (props: {
+  searchParams?: Promise<{ query?: string; page?: string }>;
+}) => {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const curentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div>
-      <div className="w-full">
-        <div className="flex w-full items-center justify-between">
+      <div className='w-full'>
+        <div className='flex w-full items-center justify-between'>
           <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
         </div>
-        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-          <Search placeholder="Search invoices" />
+        <div className='mt-4 flex items-center justify-between gap-2 md:mt-8'>
+          <Search placeholder='Search invoices' />
           <CreateInvoice />
         </div>
+        <Suspense key={query + curentPage} fallback={<InvoicesTableSkeleton />}>
+          <InvoicesTable query={query} currentPage={curentPage} />
+        </Suspense>
 
-        <div className="mt-5 flex w-full justify-center">
-
+        <div className='mt-5 flex w-full justify-center'>
+          <Pagination totalPages={totalPages} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default InvoicePage
+export default InvoicePage;
