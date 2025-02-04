2e227706dc1d5c9adc3e54549952d0f65a37a361
irfan-fauzi <irfanfauzi24@yahoo.com>
2025-02-04 23:58:52 +0700

    pagination sucess

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -14,6 +14,7 @@ const InvoicePage = async (props: {
   const query = searchParams?.query || "";
   const curentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
+  
 
   return (
     <div>
