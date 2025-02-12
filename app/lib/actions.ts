e3704e1d50ac6ccf8970c69f9119fd0e8c00bdb1
irfan-fauzi <irfanfauzi24@yahoo.com>
2025-02-12 20:39:18 +0700

    delete next-auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -117,21 +116,4 @@ export const deleteInvoice = async (id: string) => {
   revalidatePath("/dashboard/invoices");
 };
 
-export const authenticate = async (
-  prevState: string | undefined,
-  formData: FormData
-) => {
-  try {
-    await signIn("credentials", formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case "CredentialsSignin":
-          return "invalid credentials";
-        default:
-          return "Something went wrong";
-      }
-    }
-    throw error;
-  }
-};
+
