6ae756ec85beed5d4348bcd262f14db8f2588e34
irfan-fauzi <irfanfauzi24@yahoo.com>
2025-02-12 20:33:24 +0700

    auth fail in production

--- auth.config.ts
+++ auth.config.ts
@@ -7,13 +7,16 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false;
       } else if (isLoggedIn) {
+        console.log(nextUrl)
         return Response.redirect(new URL("/dashboard", nextUrl));
+        
       }
       return true;
     },
